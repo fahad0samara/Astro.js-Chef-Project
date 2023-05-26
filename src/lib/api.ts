34d18d8fa6cf52124b3a90e4bb8bdb4b34c2d921
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-05-26 14:01:49 +0300

    add

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,11 +4,13 @@ async function getFirstBlogPost() {
   const query = groq`*[_type == "post"]{
     ...,
     "author": author->,
-    "imageUrl": mainImage.asset->url
-    
+    "bodyText": body[0].children[0].text,
+
+     "imageUrls": body[_type == "image"].asset->url
   }`;
   const firstPost = await useSanityClient().fetch(query);
   return firstPost;
 }
+
 
 export {getFirstBlogPost, imageUrlBuilder};
