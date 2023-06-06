fdea03ce8b4b7765975b89b60cd11203530d22bc
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-06-06 08:36:18 +0300

    updata the file and add samthing imporow for the seo

---
+++ Type.ts
@@ -0,0 +1,32 @@
+export type PostProps = {
+  title: string;
+  author: {
+    image: string;
+    name: string;
+    bio: {
+      children: {
+        text: string;
+      }[];
+    }[];
+  };
+  publishedAt: string;
+  mainImage: string;
+  bio?: {
+    text: string;
+  };
+  tags?: string[];
+  body?: {
+    _type: string;
+    style: string;
+    listItem?: string | undefined;
+    children: {
+      text: string;
+    }[];
+  }[];
+  nutrition?: {
+    calories: number;
+    carbohydrates: number;
+    fat: number;
+    protein: number;
+  };
+};
